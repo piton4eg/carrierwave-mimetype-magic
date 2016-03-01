53664874b7b197f7f655d220b30f92d74ad567ac
Markov Alexey <piton4eg@mail.ru>
2016-03-01 23:04:41 +0400

    Fix Gemfile

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # changelog
 
+## `0.0.2`
+
+  * Fix Gemfile
+
 ## `0.0.1`
 
   * Added initial functionality.
--- lib/carrierwave-mimetype-magic/version.rb
+++ lib/carrierwave-mimetype-magic/version.rb
@@ -1,5 +1,5 @@
 module CarrierWave
   module MimetypeMagic
-    VERSION = "0.0.1"
+    VERSION = "0.0.2"
   end
 end
